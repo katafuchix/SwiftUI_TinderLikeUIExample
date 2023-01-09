5dd567fb020d75b4678c5eeebd959fc195a3aeab
katafuchix <katafuchix@gmail.com>
2023-01-09 20:36:10 +0900

    Initial Commit

---
+++ TinderLikeUIExample/TinderLikeUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderLikeUIExampleApp.swift
+//  TinderLikeUIExample
+//
+//  Created by cano on 2023/01/09.
+//
+
+import SwiftUI
+
+@main
+struct TinderLikeUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
